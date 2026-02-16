f8917b7a8eeacab0c967cc23e17158a4f01abf3d
sachinterra <sachinmakavana@gmail.com>
2026-02-16 10:33:48 +0530

    terra-code1-removed

--- Amazon/terrfor-backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-    backend "azurerm" {
-        resource_group_name   = "rg-terrfor-backend"
-        storage_account_name  = "terrforbackend"
-        container_name        = "tfstate"
-        key                   = "terraform.tfstate"
-    }
